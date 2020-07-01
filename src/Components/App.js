e5d9c7d6ebf621e76ce7e750c2e356371612a537
DaeLee <masterkorea01@naver.com>
2020-07-01 17:18:56 +0900

    Router part 2

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Router from "Components/Router";
+import Header from "Components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Router />
     </>
   );
