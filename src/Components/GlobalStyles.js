ff325026dae55915c5b37468a0a351d8e8b2897f
DaeLee <masterkorea01@naver.com>
2020-07-01 20:04:24 +0900

    Header Styled Components

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        font-size: 12px;
+        background-color: rgba(20, 20, 20, 1);
+        color: white;
+        padding-top: 50px;
+    }
+`;
+
+export default globalStyles;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,31 +3,49 @@ import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: blue;
-  }
+  margin-left: 1rem;
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+`;
 
 export default () => (
   <Header>
     <List>
-      <li>
+      <Item>
         <SLink to="/">Movies</SLink>
-      </li>
-      <li>
+      </Item>
+      <Item>
         <SLink to="/tv">TV</SLink>
-      </li>
-      <li>
+      </Item>
+      <Item>
         <SLink to="/search">Search</SLink>
-      </li>
+      </Item>
     </List>
   </Header>
 );
