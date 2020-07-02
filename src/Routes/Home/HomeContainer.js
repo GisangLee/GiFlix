474172bea1200f3fe0724390e9c65d8cf0defbad
DaeLee <masterkorea01@naver.com>
2020-07-02 14:17:08 +0900

    HomeContainer Finish

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -9,6 +10,34 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upComing },
+      } = await moviesApi.upComing();
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upComing,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "영화정보를 받아올 수 없습니다.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+      console.log(">>>영화 정보: ", this.state);
+    }
+  }
 
   render() {
     const { nowPlaying, upComing, popular, error, loading } = this.state;
