2b8374fda793d515daa02aec51ac5c60046513f5
DaeLee <masterkorea01@naver.com>
2020-07-02 18:13:21 +0900

    Prop Types

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,16 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({ nowPlaying, upComing, popular, error, loading }) =>
+  null;
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upComing: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,16 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = ({ topRated, airingToday, popular, error, loading }) =>
+  null;
+
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  airingToday: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default TVPresenter;
