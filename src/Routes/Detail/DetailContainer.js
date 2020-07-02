ffced05de36d450d8ef7d2bf9059db2015518f48
DaeLee <masterkorea01@naver.com>
2020-07-02 15:12:19 +0900

    Detail Container Finish

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+    console.log(">>>props: ", props);
+  }
 
   async componentDidMount() {
     const {
@@ -15,14 +24,33 @@ export default class extends React.Component {
       },
       history: { push },
     } = this.props;
+
+    const { isMovie } = this.state;
+    console.log(isMovie);
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
       return push("/");
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        const request = await moviesApi.movieDetail(parsedId);
+        result = request.data;
+      } else {
+        const request = await tvApi.showDetail(parsedId);
+        result = request.data;
+      }
+      console.log(">>>results: ", result);
+    } catch {
+      this.setState({ error: "결과를 찾을 수 없습니다." });
+    } finally {
+      this.setState({ loading: false, result });
     }
   }
 
   render() {
     const { result, error, loading } = this.state;
+    console.log(">>>상세 데이터: ", this.state);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
