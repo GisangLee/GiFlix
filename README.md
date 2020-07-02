84e3ae24dc50ca357f12e1983656765afca95b4a
DaeLee <masterkorea01@naver.com>
2020-07-02 13:39:03 +0900

    axios

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Practicing React and ES6 by building a Movie Discovery App.
 - [ ] TV Shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbas
+
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search Movi(Movies, TV)
---
+++ src/api.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+
+const API_KEY = "058304e64877b12849e3924d90da607b";
+const LANGUAGE = "ko-KR";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+});
+
+export const moviesApi = {
+  nowPlaying: () =>
+    api.get("movie/now_playing", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    }),
+  upComing: () =>
+    api.get("movie/upcoming", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    }),
+  popular: () =>
+    api.get("movie/popular", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    }),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+        query: encodeURIComponent(term),
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () =>
+    api.get("tv/top_rated", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    }),
+  popular: () =>
+    api.get("tv/popular", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    }),
+  airingToday: () =>
+    api.get("airing_today", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    }),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+        query: encodeURIComponent(term),
+      },
+    }),
+};
