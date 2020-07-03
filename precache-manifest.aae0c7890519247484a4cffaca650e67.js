fb6f4385a76dcc120117bcc5a6052edc6f9412e9
DaeLee <masterkorea01@naver.com>
2020-07-03 20:01:24 +0900

    Updates

---
+++ precache-manifest.aae0c7890519247484a4cffaca650e67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39d39029423d9a07d226b2eabc3d207",
+    "url": "/GiFlix/index.html"
+  },
+  {
+    "revision": "4b765a35a9da68882ddc",
+    "url": "/GiFlix/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "17fbf90bf7575e0bfa55",
+    "url": "/GiFlix/static/css/main.6b171aae.chunk.css"
+  },
+  {
+    "revision": "4b765a35a9da68882ddc",
+    "url": "/GiFlix/static/js/2.a4cfad3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GiFlix/static/js/2.a4cfad3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17fbf90bf7575e0bfa55",
+    "url": "/GiFlix/static/js/main.c8757da2.chunk.js"
+  },
+  {
+    "revision": "0965d9b9df9f47dd3625",
+    "url": "/GiFlix/static/js/runtime-main.05fa03ff.js"
+  },
+  {
+    "revision": "1f01b8803494280b5a5f22fb45bda1d5",
+    "url": "/GiFlix/static/media/noMovie.1f01b880.jpg"
+  }
+]);
