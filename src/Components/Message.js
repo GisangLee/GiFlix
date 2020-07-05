efd33f46fccf64e73be7a55f414a59d2c17e5431
DaeLee <masterkorea01@naver.com>
2020-07-05 09:55:14 +0900

    Finish

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -6,12 +6,21 @@ const Container = styled.div`
   width: 100vw;
   display: flex;
   justify-content: center;
+  @media all and (max-width: 479px) {
+    width: 100%;
+  }
 `;
 
 const Text = styled.span`
   margin: 20% auto;
   color: ${(props) => props.color};
   font-size: 24px;
+  @media all and (min-width: 1024px) and (max-width: 1366px) {
+    font-size: 28px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 16px;
+  }
 `;
 
 const Message = ({ text, color }) => (
